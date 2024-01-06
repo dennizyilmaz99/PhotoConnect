5c3ad167aace9e5c12d59bbbd722bb90f9e2560e
Denniz Yilmaz <112471865+dennizyilmaz99@users.noreply.github.com>
2024-01-06 15:02:40 +0100

    Initial Commit

---
+++ PhotoConnect/PhotoConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoConnectApp.swift
+//  PhotoConnect
+//
+//  Created by dator on 2024-01-06.
+//
+
+import SwiftUI
+
+@main
+struct PhotoConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
